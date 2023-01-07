588fc691eb306171060ce844a76532e850702883
John Doe <makaranvicheanon@gmail.com>
2023-01-08 00:56:03 +0700

    update to do

--- hook/useTodoForm.ts
+++ hook/useTodoForm.ts
@@ -5,45 +5,47 @@ import { TodoType } from "../models/ITodo";
 import { updateTodo, addToDo } from "../services/todoServices";
 
 const useTodoForm = () => {
-  const { todo, setTodo, selectedTodo } = useTodo();
+	const { todo, setTodo, selectedTodo, setSelectedTodo } = useTodo();
 
-  const edit = async () => {
-    const res = await updateTodo(selectedTodo!.id, todo);
-    return res;
-  };
+	const edit = async () => {
+		const res = await updateTodo(selectedTodo!.id, todo);
+		return res;
+	};
 
-  const create = async () => {
-    const newTodo: TodoType = {
-      id: self.crypto.randomUUID(),
-      todo: todo.toLowerCase(),
-      isCompleted: false,
-      createdAt: new Date(),
-    };
-    const res = await addToDo(newTodo);
-    return res;
-  };
+	const create = async () => {
+		const newTodo: TodoType = {
+			id: self.crypto.randomUUID(),
+			todo: todo.toLowerCase(),
+			isCompleted: false,
+			createdAt: new Date(),
+		};
+		const res = await addToDo(newTodo);
+		return res;
+	};
 
-  const submitHanlder = async (e: any) => {
-    e.preventDefault();
-    if (todo === "") {
-      alert("Cannot be empty");
-      return;
-    }
+	const submitHanlder = async (e: any) => {
+		e.preventDefault();
+		if (todo === "") {
+			alert("Cannot be empty");
+			return;
+		}
 
-    const res = !!selectedTodo ? await edit() : await create();
-    if (res === "Existed" || res === "Failed") return;
-    e.target.reset();
-    setTodo("");
-  };
+		const res = !!selectedTodo ? await edit() : await create();
+		if (res === "Existed") return;
+		if (res === "Failed") alert("todo is deleted.");
+		e.target.reset();
+		setSelectedTodo(null);
+		setTodo("");
+	};
 
-  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setTodo(e.target.value);
-  };
+	const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setTodo(e.target.value);
+	};
 
-  return {
-    changeHandler,
-    submitHanlder,
-  };
+	return {
+		changeHandler,
+		submitHanlder,
+	};
 };
 
 export default useTodoForm;
